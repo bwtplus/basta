f5eb0a4e37cf535f33d833edb4aef9ddb8f81d0e
Matej Kovalcik <matej.kovalcik@bwtplus.sk>
2018-01-04 13:29:42 +0100

    readable filenames in dist

--- webpack.config.js
+++ webpack.config.js
@@ -36,17 +36,14 @@ module.exports = {
         })
       },
       {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader'
-        ]
+        test: /\.(png|svg|jpg|gif|woff|woff2|eot|ttf|otf)$/,
+        loader: 'file-loader',
+		options: {
+		  name (file) {
+		    return '[name].[ext]'
+		  }
+		}
       }, 
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          'file-loader'
-        ]
-      },
       {
         test: /\.js$/,
         include: path.resolve(__dirname, 'static/js'),
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -38,16 +38,13 @@ module.exports = {
         })
       },
       {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader'
-        ]
-      }, 
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          'file-loader'
-        ]
+        test: /\.(png|svg|jpg|gif|woff|woff2|eot|ttf|otf)$/,
+        loader: 'file-loader',
+		options: {
+		  name (file) {
+		    return '[name].[ext]'
+		  }
+		}
       },
       {
         test: /\.js$/,
